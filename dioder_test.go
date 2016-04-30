021076c5fe56c07f0793660f6e03fc27239767c7
Jannick Fahlbusch <git@jf-projects.de>
2016-04-30 20:34:29 +0200

    Tests for dioder.SetPins()

--- dioder_test.go
+++ dioder_test.go
@@ -1,6 +1,9 @@
 package dioder
 
-import "testing"
+import (
+	"image/color"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
 	t.SkipNow()
@@ -35,5 +38,43 @@ func TestDioderPinConfiguration(t *testing.T) {
 
 	if dioder.PinConfiguration != pinConfiguration {
 		t.Errorf("Pins are not correctly configured. Gave %s, got %s", pinConfiguration, dioder.PinConfiguration)
+	}
+}
+
+func TestDioderSetAll(t *testing.T) {
+	d := New(Pins{})
+
+	d.SetAll(color.RGBA{})
+
+	if d.ColorConfiguration.A != 0 {
+		t.Error("Opacity is not correct")
+	}
+
+	if d.ColorConfiguration.R != 0 {
+		t.Error("Red is not correct")
+	}
+
+	if d.ColorConfiguration.G != 0 {
+		t.Error("Green is not correct")
+	}
+
+	if d.ColorConfiguration.B != 0 {
+		t.Error("Blue is not correct")
+	}
+}
+
+func TestDioderSetPins(t *testing.T) {
+	d := New(Pins{"1", "2", "3"})
+
+	if d.PinConfiguration.Blue != "3" {
+		t.Error("Blue pin is not correct")
+	}
+
+	if d.PinConfiguration.Green != "2" {
+		t.Error("Green pin is not correct")
+	}
+
+	if d.PinConfiguration.Red != "1" {
+		t.Error("Red pin is not correct")
 	}
 }
